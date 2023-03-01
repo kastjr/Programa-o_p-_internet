35d8c1a2a6286f176dcfabc16d40358da73ed00b
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2023-03-01 08:46:51 -0300

    Update q03.py

--- phyton e request/q03.py
+++ phyton e request/q03.py
@@ -1,23 +1,45 @@
 import requests
 import re
+from bs4 import BeautifulSoup
 
 # Solicita a URL da página do usuário
 url = input("Digite a URL da página: ")
+
+# Solicita o termo que o usuário deseja buscar
+termo = input("Digite o termo que você deseja buscar: ")
+
+# Extrai o texto da página sem as tags
+soup = BeautifulSoup(response.content, "html.parser")
+texto = soup.get_text()
+
+# Exibe os 20 primeiros e os 20 últimos caracteres do texto
+print("Primeiros 20 caracteres: ", texto[:20])
+print("Últimos 20 caracteres: ", texto[-20:])
+
 
 # Faz a solicitação HTTP da página e armazena o conteúdo HTML em uma variável
 response = requests.get(url)
 html_content = response.content.decode('utf-8')
 
-# Solicita o termo que o usuário deseja buscar
-termo = input("Digite o termo que você deseja buscar: ")
 
-# Procura o termo no conteúdo HTML usando a biblioteca re
-ocorrencias = re.findall(termo, html_content)
 
-# Exibe as ocorrências encontradas
-if ocorrencias:
-    print(f"O termo '{termo}' foi encontrado {len(ocorrencias)} vezes na página:")
-    for ocorrencia in ocorrencias:
-        print(ocorrencia)
-else:
-    print(f"O termo '{termo}' não foi encontrado na página.")
+ocorre = []
+posicao = -1
+while True:
+    posicao = texto.find(termo, posicao + 1)
+    if posicao == -1:
+        break
+        ocorre.append(posicao)
+
+        print("Ocorrências encontradas:", ocorre)
+    else:
+        print(f"O termo '{termo}' não foi encontrado na página.")
+
+    
+
+
+
+
+
+response = requests.get(url)
+
