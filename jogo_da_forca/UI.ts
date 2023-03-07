8086d9e251c8f3fef4be442ca825651547b31828
Kastier Francisco Leite Júnior <108149154+kastjr@users.noreply.github.com>
2023-03-07 16:08:48 -0300

    preenchimento da classe

--- jogo_da_forca/UI.ts
+++ jogo_da_forca/UI.ts
@@ -1,15 +1,54 @@
 class UI {
+    word: any;
+    guesses: any;
+    currentPlayer: any;
 
 
-    displayMessage(message: string): void {
+    displayMessage(message: string) {
+        console.log(message);
+      }
 
-    }
+      displayBoard() {
+        // Limpa a tela
+        console.clear();
+    
+        // Exibe a palavra a ser adivinhada com as letras adivinhadas pelo jogador atual
+        let displayWord = "";
+        for (const letter of this.word) {
+          if (this.guesses.includes(letter)) {
+            displayWord += letter;
+          } else {
+            displayWord += "_";
+          }
+        }
+        console.log(`Palavra: ${displayWord}`);
+    
+        // Exibe as partes da forca que foram desenhadas
+        console.log(`  +---+
+      |   |
+      ${this.guesses.length >= 1 ? "|   O" : "|"}
+      ${this.guesses.length >= 3 ? "|  /|\\" : this.guesses.length == 2 ? "|   |" : "|"}
+      ${this.guesses.length >= 5 ? "|  / \\" : this.guesses.length == 4 ? "|  /" : "|"}
+      |
+    =========
+    `);
+    
+        // Exibe a vez do jogador atual
+        console.log(`Vez de ${this.currentPlayer.name}`);
+      }
 
-    displayBoard(board: string[]): void {
-
-    }
-
-    getPlayerGuess(): void {
-        
-    }
+      
+      getPlayerGuess(): string {
+        let guess: string = '';
+    
+        while (!guess.match(/^[a-zA-Z]$/)) {
+          guess = readline.question(`Digite uma letra: `).toLowerCase();
+    
+          if (!guess.match(/^[a-zA-Z]$/)) {
+            console.log(`Por favor, digite apenas uma letra.`);
+          }
+        }
+    
+        return guess;
+      }
 }
